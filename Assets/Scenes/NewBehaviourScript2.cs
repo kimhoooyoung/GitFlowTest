ec01b8154947412e1df0b73894528474243c9eff
kimhooyoung <hooyoung.kim@firsthabit.com>
2026-01-23 17:59:01 +0900

    tet

--- Assets/Scenes/NewBehaviourScript2.cs
+++ Assets/Scenes/NewBehaviourScript2.cs
@@ -7,7 +7,7 @@ public class NewBehaviourScript2 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //fff
     }
 
     // Update is called once per frame
